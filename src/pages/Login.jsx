082002ba2afe54c75f0ab319e92ba9d28991109e
Priyanshu Banoudha <priyanshubanoudha8@gmail.com>
2026-07-12 20:03:29 +0530

    update page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -51,14 +51,14 @@ export default function Login({ onLogin, showToast, apiBase }) {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-6 bg-slate-950">
-      <div className={`w-full ${isRegisteringStudent ? 'max-w-2xl' : 'max-w-md'} bg-slate-900 border border-slate-800 rounded-2xl p-8 shadow-xl transition-all duration-350 ease-in-out`}>
+    <div className="min-h-screen flex items-center justify-center p-6 bg-slate-200">
+      <div className={`w-full ${isRegisteringStudent ? 'max-w-2xl' : 'max-w-md'}   rounded-2xl p-8 shadow-2xl transition-all duration-350 ease-in-out`}>
         <div className="text-center mb-8">
-          <div className="inline-flex items-center justify-center w-12 h-12 rounded-xl bg-indigo-600 text-white font-extrabold text-xl shadow-sm mb-4">
-            A
+          <div className="inline-flex items-center justify-center w-12 h-12 rounded-xl bg-indigo-600 text-white font-extrabold text-3xl shadow-sm mb-4 p-2">
+            C
           </div>
-          <h1 className="text-2xl font-bold text-white">
-            Academia Hub
+          <h1 className="text-2xl font-bold text-black">
+            Compass
           </h1>
           <p className="text-slate-405 text-sm mt-2">
             {isRegisteringStudent 
@@ -71,10 +71,10 @@ export default function Login({ onLogin, showToast, apiBase }) {
           {isRegisteringStudent ? (
             <div className="space-y-4">
               <div>
-                <label className="block text-xs font-semibold text-slate-400 uppercase tracking-wider mb-2">Full Name</label>
+                <label className="block text-xs font-semibold uppercase tracking-wider mb-2">Full Name</label>
                 <input 
                   type="text" 
-                  className="w-full px-4 py-2.5 bg-slate-950 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-600" 
+                  className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600" 
                   placeholder="e.g. John Doe"
                   value={name} 
                   onChange={(e) => setName(e.target.value)} 
@@ -84,11 +84,11 @@ export default function Login({ onLogin, showToast, apiBase }) {
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
-                  <label className="block text-xs font-semibold text-slate-400 uppercase tracking-wider mb-2">Roll Number (Unique ID)</label>
+                  <label className="block text-xs font-semibold  uppercase tracking-wider mb-2">Roll Number (Unique ID)</label>
                   <input 
                     type="text" 
-                    className="w-full px-4 py-2.5 bg-slate-950 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
-                    placeholder="e.g. CSE-2026-001"
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600" 
+                    placeholder="e.g. BE24XYZ"
                     value={rollNumber} 
                     onChange={(e) => setRollNumber(e.target.value)} 
                     required 
@@ -98,7 +98,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                   <label className="block text-xs font-semibold text-slate-405 uppercase tracking-wider mb-2">Course Program (Dept)</label>
                   <input 
                     type="text" 
-                    className="w-full px-4 py-2.5 bg-slate-950 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600"  
                     placeholder="e.g. B.Tech"
                     value={course} 
                     onChange={(e) => setCourse(e.target.value)} 
@@ -112,7 +112,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                   <label className="block text-xs font-semibold text-slate-405 uppercase tracking-wider mb-2">Branch / Major</label>
                   <input 
                     type="text" 
-                    className="w-full px-4 py-2.5 bg-slate-950 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600"  
                     placeholder="e.g. Computer Science"
                     value={branch} 
                     onChange={(e) => setBranch(e.target.value)} 
@@ -123,7 +123,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                   <label className="block text-xs font-semibold text-slate-405 uppercase tracking-wider mb-2">Current Semester</label>
                   <input 
                     type="number" 
-                    className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-505 focus:ring-1 focus:ring-indigo-550 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                    className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-505 focus:ring-1 focus:ring-indigo-550 rounded-xl  text-sm transition-all outline-none placeholder-slate-605" 
                     placeholder="1"
                     value={semester} 
                     onChange={(e) => setSemester(e.target.value)} 
@@ -139,7 +139,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                   <label className="block text-xs font-semibold text-slate-405 uppercase tracking-wider mb-2">Email Address</label>
                   <input 
                     type="email" 
-                    className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-550 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600"  
                     placeholder="e.g. john@university.edu"
                     value={email} 
                     onChange={(e) => setEmail(e.target.value)} 
@@ -150,7 +150,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                   <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Login Password</label>
                   <input 
                     type="password" 
-                    className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-550 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600"  
                     placeholder="••••••••"
                     value={password} 
                     onChange={(e) => setPassword(e.target.value)} 
@@ -161,21 +161,21 @@ export default function Login({ onLogin, showToast, apiBase }) {
 
               <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
-                  <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Student Phone</label>
+                  <label className="block text-xs font-semibold text-blackuppercase tracking-wider mb-2">Phone Number</label>
                   <input 
                     type="tel" 
-                    className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
-                    placeholder="e.g. 9876543210"
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none"  
                     value={phone} 
+                    placeholder='Phone Number'
                     onChange={(e) => setPhone(e.target.value)} 
                     required 
                   />
                 </div>
                 <div>
-                  <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Guardian Phone</label>
+                  <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Alternate Number</label>
                   <input 
                     type="tel" 
-                    className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                    className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600"  
                     placeholder="e.g. 9876543211"
                     value={guardianPhone} 
                     onChange={(e) => setGuardianPhone(e.target.value)} 
@@ -188,7 +188,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                 <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Date of Birth</label>
                 <input 
                   type="date" 
-                  className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-605" 
+                  className="w-full px-4 py-2.5  border border-slate-800 focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 rounded-xl  text-sm transition-all outline-none placeholder-slate-600" 
                   value={dob} 
                   onChange={(e) => setDob(e.target.value)} 
                   required 
@@ -201,7 +201,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                 <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Email Address</label>
                 <input 
                   type="email" 
-                  className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-505 focus:ring-1 focus:ring-indigo-505 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-505" 
+                  className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-505 focus:ring-1 focus:ring-indigo-505 rounded-xl  text-sm transition-all outline-none placeholder-slate-505" 
                   placeholder="e.g. john@university.edu"
                   value={email} 
                   onChange={(e) => setEmail(e.target.value)} 
@@ -213,7 +213,7 @@ export default function Login({ onLogin, showToast, apiBase }) {
                 <label className="block text-xs font-semibold text-slate-450 uppercase tracking-wider mb-2">Password</label>
                 <input 
                   type="password" 
-                  className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-505 focus:ring-1 focus:ring-indigo-505 rounded-xl text-white text-sm transition-all outline-none placeholder-slate-505" 
+                  className="w-full px-4 py-2.5 bg-slate-955 border border-slate-800 focus:border-indigo-505 focus:ring-1 focus:ring-indigo-505 rounded-xl text-sm transition-all outline-none placeholder-slate-505" 
                   placeholder="••••••••"
                   value={password} 
                   onChange={(e) => setPassword(e.target.value)} 
